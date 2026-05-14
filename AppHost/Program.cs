112f268f4e7b2b3cb1d56e4209bf80da32fd69e0
pnthuc <pnthuc1609@gmail.com>
2026-05-14 20:52:29 +0700

    10/10

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -2,10 +2,10 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgresUserName = builder.AddParameter("postgres-username", secret: false);
-var postgresPassword = builder.AddParameter("postgres-password", secret: true);
-var rabbitMqUserName = builder.AddParameter("rabbitmq-username", secret: false);
-var rabbitMqPassword = builder.AddParameter("rabbitmq-password", secret: true);
+var postgresUserName = builder.AddParameter("postgres-username", "myuser", publishValueAsDefault: false, secret: false);
+var postgresPassword = builder.AddParameter("postgres-password", "pnthuc1609", publishValueAsDefault: false, secret: true);
+var rabbitMqUserName = builder.AddParameter("rabbitmq-username", "guest", publishValueAsDefault: false, secret: false);
+var rabbitMqPassword = builder.AddParameter("rabbitmq-password", "guest", publishValueAsDefault: false, secret: true);
 
 // ── External Resources ────────────────────────────────────────────────────────
 var postgres = builder.AddPostgres("postgres", postgresUserName, postgresPassword, port: 5432)
@@ -15,10 +15,12 @@ var postgres = builder.AddPostgres("postgres", postgresUserName, postgresPasswor
 var rabbitmq = builder.AddRabbitMQ("rabbitmq", rabbitMqUserName, rabbitMqPassword, port: 5672)
     .WithManagementPlugin(port: 15672);
 
+var redis = builder.AddRedis("redis");
+
 // ── Databases (mỗi service dùng DB riêng) ────────────────────────────────────
 var orderDb = postgres.AddDatabase("order-db");
-var cartDb = postgres.AddDatabase("cart-db");
 var productDb = postgres.AddDatabase("product-db");
+var identityDb = postgres.AddDatabase("identity-db");
 
 // ── Services ──────────────────────────────────────────────────────────────────
 builder.AddProject<Order_API>("order-api")
@@ -28,16 +30,21 @@ builder.AddProject<Order_API>("order-api")
     .WaitFor(rabbitmq);
 
 builder.AddProject<Cart_API>("cart-api")
-    .WithReference(cartDb)
     .WithReference(rabbitmq)
-    .WithEnvironment("Services__ProductServiceUrl", "http://localhost:5133")
-    .WaitFor(cartDb)
-    .WaitFor(rabbitmq);
+    .WithReference(redis)
+    .WaitFor(rabbitmq)
+    .WaitFor(redis);
 
 builder.AddProject<Product_API>("product-api")
     .WithReference(productDb)
     .WithReference(rabbitmq)
     .WaitFor(productDb)
+    .WaitFor(rabbitmq);
+
+builder.AddProject<Identity_API>("identity-api")
+    .WithReference(identityDb)
+    .WithReference(rabbitmq)
+    .WaitFor(identityDb)
     .WaitFor(rabbitmq);
 
 builder.Build().Run();
